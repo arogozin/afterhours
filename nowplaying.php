80bf8d7828c6c919b39a261df0889e7e3b7f0a55
aleksandr <aleksandr@arg.(none)>
2013-09-21 15:00:18 -0400

    Redesigned using Object

--- nowplaying.php
+++ nowplaying.php
@@ -1,66 +1,98 @@
 <?php
 
-$default_url = (isset($argv[1]))?$argv[1]:'http://ah.fm/';
-
-$banner_current_url = 'http://www.ah.fm/stats/bannercurrent.xml';
-$forum_data_url = 'http://www.ah.fm/forum/external.php?type=XML&forumids=178&count=500';
-$base_url = 'http://www.ah.fm/forum/showthread.php';
-
-$banner_data = simplexml_load_string(file_get_contents($banner_current_url));
-$forum_data = simplexml_load_string(file_get_contents($forum_data_url));
-
-$now_playing_url = '';
-$next_playing_url = '';
-
-foreach ($forum_data as $post)
+class Link
 {
-	$now_playing = str_replace(' on AH.FM', '', $banner_data->now_playing);
-	//$now_playing = str_replace('&amp;', '&', $banner_data->now_playing);
+	protected $base_url;
+	protected $default_url;
+	protected $banner_url;
+	protected $forum_url;
+	protected $now_playing_url;
+	protected $next_playing_url;
 	
-	$next_playing = str_replace(' on AH.FM', '', $banner_data->next_playing);
-	//$next_playing = str_replace('&amp;', '&', $banner_data->next_playing);
+	protected $banner_data;
+	protected $forum_data;
 	
-	$post_title = $post->title;
-	$post_title = str_replace('<![CDATA[ ', '', $post_title);
-	$post_title = str_replace(' ]]>', '', $post_title);
-	
-	//$post_title = str_replace('&amp;', '&', $post_title);
-	
-	if (strtolower($post_title) == strtolower($now_playing))
+	public function __construct($default = 'http://ah.fm')
 	{
-		$now_playing_url = "$base_url?".$post->attributes()->id;	
-		$now_playing_url .= "-".str_replace(' ', '-', $now_playing);
-		$now_playing_url = str_replace('---', '-', $now_playing_url);
+		$this->base_url = 'http://www.ah.fm/forum/showthread.php';
+		$this->default_url = $default;
+		$this->banner_url = 'http://www.ah.fm/stats/bannercurrent.xml';
+		$this->forum_url = 'http://www.ah.fm/forum/external.php?type=XML&forumids=178&count=500';
+		
+		$this->obtainData();
+		$this->run();
+		
+		$this->finalize();
 	}
-
-	if (strtolower($post_title) == strtolower($next_playing))
+	
+	public function obtainData()
 	{
-		$next_playing_url = "$base_url?".$post->attributes()->id;
-		$next_playing_url .= "-".str_replace(' ', '-', $next_playing);
-		$next_playing_url = str_replace('---', '-', $next_playing_url);
+		$this->banner_data = 
+			simplexml_load_string(file_get_contents($this->banner_url));
+		$this->forum_data = 
+			simplexml_load_string(file_get_contents($this->forum_url));
+	}
+	
+	public function cleanTitle($title)
+	{
+		$result = $title;
+		if ($pos = strpos($title, ' on AH.FM'))
+			$result = substr($result, 0, strpos($title, ' on AH.FM'));
+		
+		if (strtotime(substr($result, 0, 10)))
+			$result = substr($result, 11);
+		
+		$result = str_replace('<![CDATA[ ', '', $result);
+		$result = str_replace(' ]]>', '', $result);
+		
+		return $result;
+	}
+	
+	public function constructUrl($id, $title)
+	{
+		$result = $this->base_url ."?$id-";
+		$result .= $this->cleanTitle($title);
+		$result = str_replace(' ', '-', $result);
+		$result = str_replace('---', '-', $result);
+	
+		return $result;
+	}
+	
+	public function finalize()
+	{
+		if (!isset($this->now_playing_url))
+			$this->now_playing_url = $this->default_url;
+		
+		if (!isset($this->next_playing_url))
+			$this->next_playing_url = $this->default_url;
+	}
+	
+	public function run()
+	{
+		foreach ($this->forum_data as $data)
+		{
+			if (strtolower($this->cleanTitle($data->title)) == strtolower($this->cleanTitle($this->banner_data->now_playing)))
+				$this->now_playing_url = $this->constructUrl($data->attributes()->id, $data->title);
+				
+			if (strtolower($this->cleanTitle($data->title)) == strtolower($this->cleanTitle($this->banner_data->next_playing)))
+				$this->next_playing_url = $this->constructUrl($data->attributes()->id, $data->title);
+		}
+	}
+	
+	public function get_now_playing_url()
+	{
+		return $this->now_playing_url;
+	}
+	
+	public function get_next_playing_url()
+	{
+		return $this->next_playing_url;
 	}
 }
 
-$now_playing_url = (!empty($now_playing_url))?$now_playing_url:$default_url;
-$next_playing_url = (!empty($next_playing_url))?$next_playing_url:$default_url;
+$link = new Link();
 
-/*
-SimpleXMLElement Object
-(
-    [now_playing] => Alex van ReeVe - Xanthe Sessions 045 on AH.FM
-    [next_playing] => Ronny K. - trance4nations 062 on AH.FM
-    [listeners] => 756
-    [url_link] => http://ah.fm/forum/forumdisplay.php?178-Livesets
-    [next_link] => empty
-)
-*/
+echo $link->get_now_playing_url() ."\n";
+echo $link->get_next_playing_url() ."\n";
 
-
-//print_r($banner_data);
-//print_r($forum_data);
-
-
-echo "<url_link>$now_playing_url</url_link>\n";
-echo "<next_link>$next_playing_url</next_link>\n";
-	
 ?>
